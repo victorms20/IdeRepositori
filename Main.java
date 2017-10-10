07be204896f5b427a5e2270438e2a93e50fdf20f
victor <gtamulty20@gmail.com>
2017-10-10 19:27:06 +0200

    Programa Numeros

--- Main.java
+++ Main.java
@@ -1,8 +1,22 @@
 package com.company;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
+        Random aleatorio = new Random();
+        int numero = 335;
+
+        List<Integer> lista_num = new ArrayList<>();
+
+        for (int i = 0; i < 500000; i++) {
+            int num_ale = aleatorio.nextInt(100000);
+            lista_num.add(num_ale);
+        }
+
+        System.out.println(lista_num.contains(numero));
 
     }
 }
